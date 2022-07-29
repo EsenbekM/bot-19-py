ed00bc223c30a02572cdc61dd851a6efc4c64c23
EsenbekM <esenbekm03@gmail.com>
2022-07-29 19:54:34 +0600

    Added inline google

--- handlers/inline.py
+++ handlers/inline.py
@@ -27,6 +27,21 @@ async def inline_youtube_handler(query: types.InlineQuery):
     ) for link in links]
     await query.answer(articles, cache_time=60, is_personal=True)
 
+async def inline_google_handler(query: types.InlineQuery):
+    text = query.query or "echo"
+    link = f"https://www.google.com/search?q={text}"
+    result_id: str = hashlib.md5(text.encode()).hexdigest()
+    articles = [types.InlineQueryResultArticle(
+        id=result_id,
+        title="Google: ",
+        url=link,
+        input_message_content=types.InputMessageContent(
+            message_text=link
+        )
+    )]
+    await query.answer(articles, cache_time=60, is_personal=True)
+
 
 def register_handlers_inline(dp: Dispatcher):
-    dp.register_inline_handler(inline_youtube_handler)
+    # dp.register_inline_handler(inline_youtube_handler)
+    dp.register_inline_handler(inline_google_handler)
